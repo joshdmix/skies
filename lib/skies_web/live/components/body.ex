ea5ab54cc78b85bf665c820a3031a1713316da01
Josh Mix <joshuadmix@gmail.com>
2022-06-21 20:01:38 -0400

    starting to visualize the bodies

--- lib/skies_web/live/components/body.ex
+++ lib/skies_web/live/components/body.ex
@@ -11,25 +11,25 @@ defmodule SkiesWeb.Live.Components.Body do
         } = assigns
       ) do
     ~H"""
-      <div>name: <%= name %></div>
-        <div>
-        km from earth: <%= distance.from_earth.km %>
-        </div>
-        <div>
-          position:
-            <div class="pl-5">
-              <div class="">constellation: <%= inspect(constellation.name) %></div>
-              <div class="">equatorial declination (degrees): <%= equatorial.declination.degrees %></div>
-              <div class="">equatorial right ascension (hours): <%= equatorial.right_ascension.hours %></div>
-          </div>
-        </div>
-        <div>
-        horizonal:
-          <div class="pl-5">
-            <div class="">altitude (degrees): <%= horizonal.altitude.degrees %></div>
-            <div class="">azimuth (degrees): <%= horizonal.azimuth.degrees %></div>
-        </div>
+      <div class="w-[8rem] h-[8rem] rounded-full border border-black text-center align-middle flex justify-items">
+        <div class="m-auto"><%= name %></div>
+        <div><%= horizonal.altitude.degrees %></div>
       </div>
     """
   end
 end
+
+# <div>
+#   position:
+#     <div class="pl-5">
+#       <div class="">constellation: <%= inspect(constellation.name) %></div>
+#       <div class="">equatorial declination (degrees): <%= equatorial.declination.degrees %></div>
+#       <div class="">equatorial right ascension (hours): <%= equatorial.right_ascension.hours %></div>
+#   </div>
+# </div>
+# <div>
+# horizonal:
+#   <div class="pl-5">
+#     <div class="">altitude (degrees): <%= horizonal.altitude.degrees %></div>
+#     <div class="">azimuth (degrees): <%= horizonal.azimuth.degrees %></div>
+# </div>
